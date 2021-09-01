e74dc1d9d4b28b27a2752f826d1d25513e97f4d7
Dinu Berinde <dinu2193@gmail.com>
2021-09-01 14:44:07 +0200

    adding background script of browser extension

---
+++ src/background.js
@@ -0,0 +1,115 @@
+const browser = require("webextension-polyfill")
+const { v4: uuidv4 } = require('uuid')
+
+
+/**
+ * The connection handler for Hotwallet.
+ * @param sendResponse the function to be invoked with the results
+ */
+const connectionHandler = (sendResponse) => {
+    browser.storage.local.get('account').then(result => {
+
+        if (result && result.account) {
+            if (result.account.sessionPeriod && new Date() > new Date(result.account.sessionPeriod)) {
+                sendResponse({
+                    error: "Please login to HotWallet"
+                })
+            } else {
+                browser.storage.local.get('transactionMap').then(result => {
+                    if (!result || !result.transactionMap) {
+                        browser.storage.local.set({transactionMap: {}})
+                    }
+                })
+
+                sendResponse({
+                    account: {
+                        name: result.account.name,
+                        address: result.account.address,
+                        balance: result.account.balance
+                    }
+                })
+            }
+        } else {
+            sendResponse({
+                error: "Please create an account using Hotwallet"
+            })
+        }
+    }).catch(() => {
+        sendResponse({
+            error: "Please create an account using Hotwallet"
+        })
+    })
+}
+
+/**
+ * The transaction handler.
+ * @param message the message of the transaction
+ */
+const transactionHandler = (message) => {
+
+    browser.storage.local.get('transactionMap').then(result => {
+        if (result && result.transactionMap) {
+            result.transactionMap[message.hotmoka.transaction.uuid] = message.hotmoka.transaction
+
+            browser.storage.local.set({...result}).then(() => {
+                browser.windows.create({
+                    type: 'popup',
+                    width: 360,
+                    url: browser.runtime.getURL(
+                        "app/popup.html#/transaction:" + message.hotmoka.transaction.uuid
+                    )
+                })
+            })
+        }
+    })
+}
+
+/**
+ * Removes from storage the transaction identified by the given uuid.
+ * @param uuid the uuid of the transaction
+ */
+const removeTransaction = (uuid) => {
+    browser.storage.local.get('transactionMap').then(result => {
+        if (result && result.transactionMap && result.transactionMap.hasOwnProperty(uuid)) {
+            delete result.transactionMap[uuid]
+            browser.storage.local.set({...result})
+        }
+    })
+}
+
+/**
+ * The transaction map of Hotwallet.
+ */
+const transactionMap = new Map()
+
+/**
+ * The runtime message listener of the browser which
+ * listens to messages from the content script or the Hotwallet app itself.
+ */
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
+
+    if (message && message.hotmoka) {
+        if (message.hotmoka.type === 'connect') {
+            connectionHandler(sendResponse)
+
+        } else if (message.hotmoka.type === 'transaction') {
+            const uuid = uuidv4()
+            message.hotmoka.transaction.uuid = uuid
+            transactionMap.set(uuid, {sendResponse: sendResponse})
+            transactionHandler(message)
+
+        } else if (message.hotmoka.type === 'transactionResult') {
+            const uuid = message.hotmoka.transactionResult.uuid
+            transactionMap.get(uuid).sendResponse({
+                error: message.hotmoka.transactionResult.error,
+                status: message.hotmoka.transactionResult.status,
+                storageValue:  message.hotmoka.transactionResult.storageValue
+            })
+            transactionMap.delete(uuid)
+            removeTransaction(uuid)
+        }
+
+        // async
+        return true
+    }
+})
