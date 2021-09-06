1de18e382a43ff2937358a143a426683b13b5e10
Dinu Berinde <dinu2193@gmail.com>
2021-09-06 09:30:10 +0200

    implementing home component

--- src/app/components/Home.vue
+++ src/app/components/Home.vue
@@ -1,69 +1,67 @@
 <template>
-  <div class="text-center">
-    <h6 class="mt-4 mb-4" v-if="name">{{name}}</h6>
-    <p class="text-secondary" v-if="account">{{account.transaction.hash}}#{{parseInt(account.progressive).toString(16)}}</p>
-    <hr/>
+  <div class="content text-center">
+    <div v-if="account">
+      <h6 class="mt-4 mb-4">{{ account.name }}</h6>
+      <p class="address" v-if="account.reference">
+        {{ account.reference.transaction.hash }}#{{ parseInt(account.reference.progressive).toString(16) }}</p>
+      <hr/>
 
-    <div>
-      <p class="text-dark">Balance</p>
-      <h4 class="text-success">{{balance ? balance : '0'}} Mokas </h4>
-      <h4 class="text-danger">{{balanceRed ? balanceRed : '0'}} Mokas </h4>
+      <div v-if="account.balance">
+        <p class="text-dark">Balance</p>
+        <h4 class="text-success">{{ account.balance }} Mokas </h4>
+        <h4 class="text-danger">{{ account.balanceRed ? account.balanceRed : '0' }} Mokas </h4>
+      </div>
+
+      <hr/>
+
+      <b-button variant="outline-danger" @click="onLogoutClick">Logout</b-button>
     </div>
-
-    <hr/>
-
-    <b-button variant="outline-danger" @click="onLogoutClick">Logout</b-button>
-
   </div>
 </template>
 
 <script>
-import {RemoteNode} from "hotweb3";
-import {EventBus, getSessionPeriod} from "../internal/utils";
+import {RemoteNode, StorageReferenceModel} from "hotweb3";
+import {EventBus, showErrorToast} from "../internal/utils";
 
 export default {
   name: "Home",
   data() {
     return {
-      name: null,
-      account: null,
-      balance: 0,
-      balanceRed: 0,
-      nonce: 0
+      account: {
+        balance: 0,
+        balanceRed: 0,
+        nonce: 0
+      }
     }
   },
   methods: {
-    getAccountInfo(accountAddress) {
+    getAccountInfo(accountReference) {
       EventBus.$emit('showSpinner', true)
-      new RemoteNode(this.$blockchainConfig.remoteNodeUrl).getState(accountAddress).then(result => {
+      new RemoteNode(this.$blockchainConfig.remoteNodeUrl).getState(StorageReferenceModel.newStorageReference(accountReference.transaction.hash)).then(result => {
         EventBus.$emit('showSpinner', false)
 
         const updates = result.updates
         updates.forEach(update => {
           if (update.field && update.field.name) {
-            if (this.hasOwnProperty(update.field.name)) {
-              this[update.field.name] = update.value.value
+            if (this.account.hasOwnProperty(update.field.name)) {
+              console.log('updating', update.field.name)
+              this.account[update.field.name] = update.value.value
             }
           }
         })
 
-        this.$browser.storage.local.get('account').then(result => {
-          if (result && result.account) {
-            result.account.balance = this.balance
-            this.$browser.storage.local.set({
-              ...result
-            })
+        this.$browser.setToStorage({
+          account: {
+            ...this.account,
+            balance: this.account.balance,
+            balanceRed: this.account.balanceRed
           }
         })
 
+        console.log('setting to storage')
       }).catch(err => {
         EventBus.$emit('showSpinner', false)
-        this.$bvToast.toast("Errore durante il caricamento dell'account", {
-          title: 'Account',
-          autoHideDelay: 5000,
-          variant: 'danger',
-          solid: true
-        })
+        showErrorToast(this, 'Account', 'Cannot retrieve account details')
       })
     },
     onLogoutClick() {
@@ -80,11 +78,11 @@ export default {
     }
   },
   created() {
-    this.$browser.storage.local.get('account').then(result => {
-      if (result && result.account) {
-        this.name = result.account.name
-        this.account = result.account.address
-        this.getAccountInfo(this.account)
+    this.$browser.getFromStorage('account', account => {
+      if (account) {
+        this.account = {...this.account, ...account}
+        console.log('this,', this.account)
+        this.getAccountInfo(this.account.reference)
       }
     })
   }
@@ -92,7 +90,4 @@ export default {
 </script>
 
 <style scoped>
-p {
-  word-break: break-word !important;
-}
 </style>
