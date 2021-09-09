b21b2d95a01e95c3875178333a1909f8a208e0bc
Dinu Berinde <dinu2193@gmail.com>
2021-09-09 21:54:43 +0200

    adding AES encryption of data

---
+++ src/internal/utils.js
@@ -0,0 +1,46 @@
+/**
+ * Method that encrypts a plain text with a password.
+ * @param plainText the plain text
+ * @param password the password
+ * @return {Promise<string>} a promise that resolves to the cipher text
+ */
+export const aesEncrypt = async (plainText, password) => {
+    const passwordBytes = new TextEncoder().encode(password);
+    const passwordHash = await crypto.subtle.digest('SHA-256', passwordBytes);
+
+    const iv = crypto.getRandomValues(new Uint8Array(12));
+    const alg = { name: 'AES-GCM', iv: iv };
+    const encryptionKey = await crypto.subtle.importKey('raw', passwordHash, alg, false, ['encrypt']);
+
+    const dataBytes = new TextEncoder().encode(plainText);
+    const cipherBuffer = await crypto.subtle.encrypt(alg, encryptionKey, dataBytes);
+
+    const cipherBytesArray = Array.from(new Uint8Array(cipherBuffer));
+    const cipherString = cipherBytesArray.map(byte => String.fromCharCode(byte)).join('');
+    const cipherBase64 = btoa(cipherString);
+
+    const ivHex = Array.from(iv).map(b => ('00' + b.toString(16)).slice(-2)).join('');
+    return ivHex + cipherBase64
+}
+
+/**
+ * Method that decrypts an encrypted plain text with a password.
+ * @param encryptedText the encryptedText plain text
+ * @param password the password
+ * @return {Promise<string>} a promise that resolves to the plain text
+ */
+export const aesDecrypt = async (encryptedText, password) => {
+    const passwordBytes = new TextEncoder().encode(password);
+    const passwordHash = await crypto.subtle.digest('SHA-256', passwordBytes);
+
+    const iv = encryptedText.slice(0,24).match(/.{2}/g).map(byte => parseInt(byte, 16));
+    const alg = { name: 'AES-GCM', iv: new Uint8Array(iv) };
+    const decryptionKey = await crypto.subtle.importKey('raw', passwordHash, alg, false, ['decrypt']);
+
+    const cipherText = atob(encryptedText.slice(24));
+    const cipherBytes = new Uint8Array(cipherText.match(/[\s\S]/g).map(ch => ch.charCodeAt(0)));
+
+    const plainBuffer = await crypto.subtle.decrypt(alg, decryptionKey, cipherBytes);
+    const plaintext = new TextDecoder().decode(plainBuffer);
+    return plaintext
+}
