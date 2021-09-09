ce14d3639698145bd70ee8acf0a690af1cd2776c
Dinu Berinde <dinu2193@gmail.com>
2021-09-09 21:55:29 +0200

    adding a storage api for app

--- src/app/internal/StorageApi.js
+++ src/app/internal/StorageApi.js
@@ -8,42 +8,76 @@ export class StorageApi {
     }
 
     /**
-     * Sets the data to storage.
-     * @param data the data
-     * @return {Promise<boolean>} a promise that resolves to the result of the operation
+     * It initializes the local storage and the in memory storage.
+     * @param password the password to encrypt/decrypt the data of local storage
+     * @return {Promise<unknown>} a promise that resolves to void
      */
-    setToStorage(data) {
-        return new Promise(resolve => {
-            this.browser.runtime.sendMessage({
-                hotmoka: {
-                    type: 'storage-set',
-                    data: data
-                }
-            }).then(committed => {
-                resolve(committed)
-            }).catch(() => resolve(false))
-        }).catch(() => {})
+    initStore(password) {
+        return this.browser.runtime.sendMessage({
+            hotmoka: {
+                type: 'store-init',
+                password: password
+            }
+        })
     }
 
     /**
-     * It returns the data from storage.
+     * It publishes an object to the in memory store.
+     * @param data the data object
+     * @return {Promise<boolean>} a promise that resolves to the result of the operation
+     */
+    setToStore(data) {
+        return this.browser.runtime.sendMessage({
+            hotmoka: {
+                type: 'store-set',
+                data: data
+            }
+        })
+    }
+
+    /**
+     * It returns the data from the in memory store.
+     * @param key an optional key that if specified returns the data associated with that key
+     * @return {Promise<unknown>} a promise that resolves to the result data
+     */
+    getStore(key) {
+        return new Promise((resolve, reject) => {
+            this.browser.runtime.sendMessage({
+                hotmoka: {
+                    type: 'store-get'
+                }
+            }).then(result => {
+                resolve(key && result[key] ? result[key] : result)
+            }).catch(err => reject(err))
+        })
+    }
+
+    /**
+     * It publishes an object to the browser's local storage.
+     * @param data the data object
+     * @return {Promise<boolean>} a promise that resolves to the result of the operation
+     */
+    setToLocalStorage(data) {
+        return this.browser.runtime.sendMessage({
+            hotmoka: {
+                type: 'storage-local-set',
+                data: data
+            }
+        })
+    }
+
+    /**
+     * It returns the data from the browser's local storage.
      * @param key an optional key that if specified returns the data associated with that key
      * @return {Promise<unknown>} a promise that resolves to the result data from storage
      */
-    getStorage(key) {
-        return new Promise(resolve => {
-            this.browser.runtime.sendMessage({
-                hotmoka: {
-                    type: 'storage-get'
-                }
-            }).then(result => {
-                if (key) {
-                    resolve(result[key])
-                } else {
-                    resolve(result)
-                }
-            }).catch(() => resolve(null))
-        }).catch(() => {})
+    getLocalStorage(key) {
+        return this.browser.runtime.sendMessage({
+            hotmoka: {
+                type: 'storage-local-get',
+                key: key
+            }
+        })
     }
 
     /**
@@ -51,7 +85,16 @@ export class StorageApi {
      * @return {Promise<unknown>} a promise that resolves to the current account object
      */
     getCurrentAccount() {
-        return this.getStorage('account')
+        return this.getStore('account')
+    }
+
+    /**
+     * Sets the network object.
+     * @param network the network object
+     * @return {Promise<boolean>} a promise that resolves the operation result
+     */
+    setNetwork(network) {
+        return this.setToLocalStorage({network: network})
     }
 
     /**
@@ -59,7 +102,7 @@ export class StorageApi {
      * @return {Promise<unknown>} a promise that resolves to the network object
      */
     getNetwork() {
-        return this.getStorage('network')
+        return this.getLocalStorage('network')
     }
 
     /**
@@ -68,17 +111,25 @@ export class StorageApi {
      */
     getAuthentication() {
         return new Promise(resolve => {
-            this.getCurrentAccount().then(account => {
+            this.getStore().then(store => {
+
                 const result = {
                     authenticated: false,
                     hasAccount: false
                 }
-                if (account) {
+
+                if (store && store.checked) {
                     result.hasAccount = true
-                    result.authenticated = account.logged
+                    if (store.account) {
+                        result.authenticated = store.account.logged
+                    }
                 }
+
                 resolve(result)
-            })
-        }).catch(err => console.error(err))
+            }).catch(() => resolve({
+                authenticated: false,
+                hasAccount: false
+            }))
+        }).catch(() => {})
     }
 }
