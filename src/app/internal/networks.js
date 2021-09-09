cca190b9fb8c688e28ede094142738cb8ebe81d3
Dinu Berinde <dinu2193@gmail.com>
2021-09-09 23:45:45 +0200

    adding networks

---
+++ src/app/internal/networks.js
@@ -0,0 +1,13 @@
+export const networks = [
+    { value: 'panarea', text: 'panarea.hotmoka.io', protocol: 'http', url: 'http://panarea.hotmoka.io' },
+    { value: 'customNetwork', text: 'Connect to custom network' }
+]
+
+export const getNetwork = value => {
+    const _networks = networks.filter(network => network.value === value)
+    if (_networks.length > 0) {
+        return _networks[0]
+    } else {
+        return null
+    }
+}
