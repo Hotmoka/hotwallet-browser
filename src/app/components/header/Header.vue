aecce05e6a591bc055491deb054be16eff6e96a0
Dinu Berinde <dinu2193@gmail.com>
2022-01-25 16:28:41 +0100

    header refactor

--- src/app/components/header/Header.vue
+++ src/app/components/header/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="header">
+    <div class="d-flex align-items-center">
       <img src="../../assets/img/hotmoka_logo.png" height="32" width="32" alt="hotmoka" @click="onHeaderImageClick">
       <h6 class="hotwallet-title text-secondary">Hotwallet</h6>
+    </div>
 
-    <div class="w-100 text-right" v-if="currentNetwork">
+    <div class="w-100 text-right" v-if="showNetwork && currentNetwork">
       <div class="network-badge text-secondary"><b-icon icon="globe" variant="info"></b-icon> {{ currentNetwork.text }}</div>
     </div>
 
@@ -13,12 +15,22 @@
 <script>
 import {EventBus, showErrorToast} from "../../internal/utils";
 import {replaceRoute} from "../../internal/router";
+import CustomNetworkModal from "../features/CustomNetworkModal";
 
 export default {
   name: "Header",
+  components: {CustomNetworkModal},
   data() {
     return {
+      showNetwork: true,
       currentNetwork: null
+    }
+  },
+  watch: {
+    $route(to, from) {
+      if (to && to.path) {
+        this.showNetwork = to.path !== '/create-key' && to.path !== '/import-account'
+      }
     }
   },
   methods: {
@@ -33,18 +45,18 @@ export default {
         replaceRoute('/home')
       }
     },
-    setCurrentNetwork: async function () {
+    initNetwork: async function () {
       try {
         this.currentNetwork = await this.$storageApi.getCurrentNetwork()
-        this.$network.set(this.currentNetwork)
       } catch (e) {
         showErrorToast(this, 'Network', 'Network error')
       }
     }
   },
   created() {
+    this.showNetwork = this.$route.path !== '/create-key' && this.$route.path !== '/import-account'
     EventBus.$on('networkChange', network => this.currentNetwork = network)
-    this.setCurrentNetwork()
+    this.initNetwork()
   }
 }
 </script>
@@ -56,6 +68,7 @@ export default {
   height: 62px;
   display: flex;
   align-items: center;
+  justify-content: space-between;
   background-color: rgba(186, 239, 233, 0.2);
   padding: 1rem
 }
@@ -65,19 +78,16 @@ export default {
   cursor: pointer;
 }
 
-.h-network {
-  width: auto !important;
-}
-
 .network-badge {
   display: inline-block;
   padding: 8px 12px;
   border: 1px solid theme-color('info');
   background-color: #fff;
-  color: black;
   border-radius: 100px;
   font-size: 14px;
   font-weight: 500;
+  height: 42px;
+  max-width: 260px;
 }
 
 .hotwallet-title {
